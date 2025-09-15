ad7d1aa979d1e3e0003220fc7ad5bc2d87c831bb
socrateswebservices-dot <socrateswebservices@gmail.com>
2025-09-15 15:31:38 +0100

    Update get-headlines.js

--- netlify/functions/get-headlines.js
+++ netlify/functions/get-headlines.js
@@ -11,7 +11,7 @@ const parser = new xml2js.Parser({
 
 exports.handler = async function () {
   const newsOutlets = {
-    "daily-sun": "https://www.vanguardngr.com/feed/",
+    "vanguard": "https://www.vanguardngr.com/feed/",
     "business-day": "https://businessday.ng/feed/",
     "tribune": "https://tribuneonlineng.com/feed/",
     "daily-trust": "https://dailytrust.com.ng/feed/",
